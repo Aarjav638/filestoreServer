1296ec3f3d8b1fc464665ed5d0f9cb15f8d8a5ea
Aarjav Jain <anshjain638@gmail.com>
2024-11-12 15:51:06 +0530

    added home route to aws

--- routes/awsRoutes.js
+++ routes/awsRoutes.js
@@ -2,5 +2,9 @@ const express = require('express');
 const router = express.Router();
 const fetchContentcontroller=require('../controllers/aws');
 // router.post('/',fetchContentcontroller);
+router.get('/',(req,res)=>{
+    res.send('Hello from aws')
+}
+)
 router.post('/fetch-content',fetchContentcontroller);
 module.exports=router;
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const cors=require('cors');
 const app=express();
 app.use(express.json());
 app.use(cors());
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
 
 const port=3000;
 
-// app.use(express.json());
 
 app.use('/aws',require('./routes/awsRoutes'));
 
